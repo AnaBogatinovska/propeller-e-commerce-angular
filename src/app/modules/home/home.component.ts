cb48c5fa16d7e6764184b2e5841782311ddf92a4
Ana <bogatinovska14@gmail.com>
2023-11-15 19:50:43 +0100

    Add pagination to list products

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Actions, ofType } from '@ngrx/effects';
 import { Store } from '@ngrx/store';
@@ -17,24 +18,35 @@ export class HomeComponent implements OnInit, OnDestroy {
   public collections$ = this.store.select(selectCollections);
   public spinner: boolean = false;
   public sortBy = null
+  public skip = 0;
+  public take = 0;
   public selectedCollectionId = null;
+
+  // Pagination
+  public totalItemsLength: number = 0;
+  public pageSize: string = '15';
+  public pageIndex: number = 1;
+  public totalPages: number = 1;
+  public isPaginationDisabled: boolean = false;
+  @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
+  @ViewChild('paginationSelector') paginationSelector: ElementRef<Element>;
 
   private subSearch: Subscription;
   private subSearchFailed: Subscription;
 
-  constructor(private store: Store, 
-    private router: Router, 
-    private route: ActivatedRoute, 
+  constructor(private store: Store,
+    private router: Router,
+    private route: ActivatedRoute,
     private actions$: Actions
-    ) { }
+  ) { }
 
   ngOnInit(): void {
-    const id = this.route.snapshot.queryParams?.collectionId;
-    const sortBy = this.route.snapshot.queryParams?.sortBy;
-    if (id) {
-      this.selectedCollectionId = id
-    }
-    this.sortBy = sortBy ?? null
+    const storageFilters = this.filters
+    this.selectedCollectionId = storageFilters?.collectionId || this.route.snapshot.queryParams?.collectionId || 0;
+    this.sortBy = storageFilters?.sortBy || this.route.snapshot.queryParams?.sortBy;
+    this.skip = storageFilters?.skip || this.route.snapshot.queryParams?.skip || null;
+    this.take = storageFilters?.take || this.route.snapshot.queryParams?.take || this.pageSize;
+    this.pageIndex = storageFilters?.pageIndex || 1
     this.searchProductsByCollection();
     this.loadCollections();
     this.actions();
@@ -45,6 +57,10 @@ export class HomeComponent implements OnInit, OnDestroy {
     tryUnsubscribe(this.subSearchFailed);
   }
 
+  get filters(){
+    return JSON.parse(localStorage.getItem('filters'))
+  }
+
   private loadCollections(): void {
     this.store.dispatch(productsActions.GetCollections({ payload: { opts: { take: 100 } } }))
   }
@@ -52,9 +68,11 @@ export class HomeComponent implements OnInit, OnDestroy {
   private actions() {
     this.subSearch = this.actions$.pipe(ofType(productsActions.SEARCH_SUCCESS)).subscribe(action => {
       this.spinner = false;
+      this.isPaginationDisabled = false;
     })
     this.subSearchFailed = this.actions$.pipe(ofType(productsActions.REQUEST_FAILED)).subscribe(action => {
       this.spinner = false;
+      this.isPaginationDisabled = false;
     })
   }
 
@@ -65,19 +83,39 @@ export class HomeComponent implements OnInit, OnDestroy {
   public onCollectionSelect(event) {
     const id = event.value;
     this.selectedCollectionId = id
-    this.router.navigate([], { queryParams: { collectionId: id, sortBy:  this.sortBy } })
+    this.skip = null;
+    this.take = +this.pageSize;
     this.searchProductsByCollection()
   }
 
   public sortBySelection(event) {
-    console.log(event, this.selectedCollectionId)
     this.sortBy = event.value
-    this.router.navigate([], { queryParams: { collectionId: this.selectedCollectionId, sortBy: event?.value } })
     this.searchProductsByCollection()
   }
 
   private searchProductsByCollection() {
+    this.router.navigate([], { queryParams: { collectionId: this.selectedCollectionId  == 0 ? null: this.selectedCollectionId, sortBy: this.sortBy, skip: +this.skip == 0 ? null: +this.skip , take: +this.take ?? null } })
+    const filters = { collectionId: this.selectedCollectionId || null, sort: { price: this.sortBy }, skip: +this.skip ?? null, take: +this.take ?? null }
+    const storageFilters = { pageIndex: this.pageIndex, collectionId: this.selectedCollectionId, sort: { price: this.sortBy }, skip: +this.skip ?? null, take: +this.take ?? null }
+    this.saveFilters(storageFilters)
     this.spinner = true;
-    this.store.dispatch(productsActions.Search({ payload: { opts: { collectionId: this.selectedCollectionId, sort: { price: this.sortBy} } } }))
+    this.store.dispatch(productsActions.Search({ payload: { opts: filters } }))
   }
+
+  private saveFilters(filters) {
+    localStorage.setItem('filters', JSON.stringify(filters))
+  }
+
+  public onChangePage(data) {
+    if (data.pageIndex < data.length) {
+      const skip = data.pageIndex * data.pageSize;
+      const take = data.pageSize;
+      this.skip = skip;
+      this.take = take;
+      this.pageIndex = data.pageIndex;
+      this.isPaginationDisabled = true;
+      this.searchProductsByCollection()
+    }
+  }
+
 }
